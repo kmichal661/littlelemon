05da167b5ba3ae9ed34ff90ab445811297943197
kmichal661 <kmichal661@gmail.com>
2023-01-06 09:09:55 +0100

    staring the capstone project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import Header from './Views/Header';
+import Footer from './Views/Footer';
+import Main from './Views/Main';
+import Nav from './Views/Nav';
 
 function App() {
   return (
     <div className="App">
+      <>
+      <Header />
+
+      <Nav />
+
+      <Main />
+
+      <Footer />
+      </>
     </div>
   );
 }
---
+++ src/Views/Header.js
@@ -0,0 +1,11 @@
+import Logo from '../assets/Logo.svg'
+
+function Header(){
+    return(
+        <header>
+            <img src={Logo} alt="logo" />   
+        </header>
+    )
+}
+
+export default Header
