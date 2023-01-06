c2d8573c523e8364e552ea53f24ddc36ee13f06c
kmichal661 <kmichal661@gmail.com>
2023-01-06 13:48:50 +0100

    adding react-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
+import Home from './Views/Home';
+import {Routes, Route} from 'react-router-dom'
 import Header from './Views/Header';
 import Footer from './Views/Footer';
-import Main from './Views/Main';
 import Nav from './Views/Nav';
 
 function App() {
   return (
     <div className="App">
-      <>
       <div className="topSecion">
-        <Header />
-        <Nav />
-      </div>
-      
+          <Header />
+          <Nav />
+        </div>
 
-      <Main className="mainSection" />
 
-      <Footer className="footerSection" />
-      </>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/booking" element={<div>Booking</div>} />
+
+      </Routes>
+
+
+      <Footer />
     </div>
   );
 }
---
+++ src/Views/Home.js
@@ -0,0 +1,17 @@
+import Header from "./Header"
+import Nav from "./Nav"
+import Main from "./Main"
+import Footer from "./Footer"
+
+export default function Home(){
+    return(
+        <>
+        
+        
+  
+        <Main className="mainSection" />
+  
+        {/* <Footer className="footerSection" /> */}
+        </>
+    )
+}
