a9c76e5c055f634680481e2a41a1297b8a364459
kmichal661 <kmichal661@gmail.com>
2023-01-07 10:32:06 +0100

    adding tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,45 @@
 import { render, screen } from "@testing-library/react";
 import BookingForm from './components/BookingForm';
-
-test('Renders the BookingForm heading', () => {
-  const mockedUsedNavigate = jest.fn();
-    jest.mock('react-router-dom', () => ({
-        ...jest.requireActual('react-router-dom'),
-        useNavigate: () => mockedUsedNavigate
-      }));
-    render(<BookingForm />);
-    const headingElement = screen.getByText("Book Now");
+import {BrowserRouter} from 'react-router-dom'
+import Nav from './Views/Nav'
+import MainHead from './components/MainHead'
+test('Renders the MainHead heading', () => {
+    render(
+      <MainHead />
+    );
+    const headingElement = screen.getByText("Little Lemon");
+    const subHeadingElement = screen.getByText("Chicago")
     expect(headingElement).toBeInTheDocument();
+    expect(subHeadingElement).toBeInTheDocument();
 })
+
+test("Test The Navigation Elements", () => {
+  render(
+    <BrowserRouter><Nav /></BrowserRouter>
+  
+  )
+  const homeElement = screen.getByText("Home")
+  const aboutElement = screen.getByText("About")
+  const menuElement = screen.getByText("Menu")
+  const reservationsElement = screen.getByText("Reservations")
+  const orderOnlineElement = screen.getByText("Order Online")
+  const loginElement = screen.getByText("Login")
+  expect(homeElement).toBeInTheDocument()
+  expect(aboutElement).toBeInTheDocument()
+  expect(menuElement).toBeInTheDocument()
+  expect(reservationsElement).toBeInTheDocument()
+  expect(orderOnlineElement).toBeInTheDocument()
+  expect(loginElement).toBeInTheDocument()
+})
+
+test("Test The Booking Form", () => {
+  render(
+    <BrowserRouter>
+      <BookingForm />
+    </BrowserRouter>
+  )
+  const submit = screen.getByText("Book Now")
+  expect(submit).toBeInTheDocument()
+
+} )
+
